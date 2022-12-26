1db93f3e2e0eb6a2d7493eb223542d0a1198e22c
Joao <jvfarias47@gmail.com>
2022-12-25 22:50:22 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ButtonContainer } from "./styles";
+
+interface IButtonProps {
+  title: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  isDisabled: boolean;
+}
+
+const Button = ({ title, onClick, isDisabled }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={isDisabled}>{title}</ButtonContainer>;
+};
+
+export default Button;
